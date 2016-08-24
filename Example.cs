458983134db04cc93cd23b6fa57e61af2de3ae93
wangengzheng <1021349117@qq.com>
2016-08-24 18:06:53 +0800

    PinYinUtils
    PinYinUtils

---
+++ Example.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PinYinUtils
+{
+	public class Examples
+	{
+		/// <summary>
+		/// get frist Pinyin
+		/// </summary>
+		public void Method1()
+		{
+			China.PinYin.PinYinUtils.FirstPinYin("中");
+			//return : Z (upper)
+		}
+
+		/// <summary>
+		/// get hexstr
+		/// </summary>
+		public void GetHexStr()
+		{
+			char ch = "拼音"[0];
+
+			string key = string.Format("{0:X}", (int)ch).ToUpper() ?? "";
+
+			//reuslt :62FC
+		}
+
+		/// <summary>
+		///HanZi_UTF8_Range
+		/// </summary>
+		public void HanZi_UTF8_Range()
+		{
+			//pinyin  //[\u4E00-\u9FA5];
+			// int value range   19968  40865
+
+			for (int begin = 19968; begin <= 40895; begin++)
+			{
+				//hanzi
+				string utf8Str = Char.ConvertFromUtf32(begin);
+				//hexstr
+				string hexStr = begin.ToString("X8").Substring(4);
+
+				Console.WriteLine(hexStr + "，" + utf8Str);
+
+
+			}
+
+		}
+
+		/// <summary>
+		/// Create_Xml_File
+		/// </summary>
+		public void Create_Xml_File()
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int begin = 19968; begin <= 40895; begin++)
+			{
+				string utf8Str = Char.ConvertFromUtf32(begin);
+
+				string hexStr = begin.ToString("X8").Substring(4);
+
+				Console.WriteLine(hexStr + "_____________" + utf8Str);
+
+				sb.AppendFormat("<item hexStr=\"{0}\" pinyinStr=\"{1}\" utf8Str=\"{2}\" /> \n", hexStr, ConvertToPinYin(utf8Str), utf8Str);
+
+				//sb.AppendLine("<item hexStr=\"" + hexStr + "\" pinyinStr=\"" + ConvertToPinYin(utf8Str) + "\" utf8Str=\"" + utf8Str + "\" />");
+
+				//sb.AppendLine("<p>hexStr_________" + hexStr + "______________pinyinStr_____________" + ConvertToPinYin(utf8Str) + "_________________utf8Str_______________" + utf8Str + "</p>");
+			}
+
+
+			System.IO.File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "pinyin.xml", sb.ToString(), Encoding.UTF8);
+		
+		}
+
+		/// <summary>
+		/// ConvertToPinYin
+		/// <remarks>Idea source Idea source Idea source</remarks>
+		/// </summary>
+		/// <param name="utf8Str"></param>
+		/// <returns></returns>
+		public string ConvertToPinYin(string utf8Str)
+		{
+			//https://www.nuget.org/packages?q=pinyin
+
+			//method 1 Install-Package Microsoft.International.Converters.PinYinConverter			
+
+			//method 2 Install-Package pinyin4net
+
+
+			return string.Empty;
+		}
+	}
+}
---
+++ PinYinUtils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+using System.Web.Caching;
+using System.Xml.Linq;
+
+namespace China.PinYin
+{
+	public class PinYinUtils
+	{
+		private static string FilePath;
+		const string CacheKey = "PinYinCache";
+		
+		static PinYinUtils()
+		{
+			FilePath = AppDomain.CurrentDomain.BaseDirectory + "\\PINYINS.xml";			
+		}
+
+		public static string FirstPinYin(string @string)
+		{
+
+			if (string.IsNullOrEmpty(@string))
+				return "Z";
+
+			char ch = @string.TrimStart()[0];
+			string key = string.Format("{0:X}", (int)ch).ToUpper() ?? "";
+
+			var dict = PinYinUtils.GetCache();
+
+			string pinyin = null;
+			//http://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,bcd13bb775d408f1
+			if (dict.TryGetValue(key, out pinyin))
+			{
+				return pinyin.Length > 0 ? pinyin.Substring(0, 1).ToUpper() : "";
+			}
+
+
+			return "Z";
+		}
+
+
+
+		/// <summary>
+		/// web
+		/// </summary>
+		/// <returns></returns>
+		public static Dictionary<string, string> GetCache()
+		{
+			if (HttpContext.Current.Cache[CacheKey] != null)
+			{
+				return (Dictionary<string, string>)HttpContext.Current.Cache[CacheKey];
+			}
+			else
+			{
+				XDocument xml = XDocument.Load(PinYinUtils.FilePath);
+
+				var PinYins = from item in xml.Element("PINYINS").Elements()
+							select new
+							{
+								hexStr = item.Attribute("hexStr").Value,
+								firstPinYin = item.Attribute("firstPinYin").Value,
+								utf8Str = item.Attribute("utf8Str").Value,
+								allPinYin = item.Attribute("allPinYin").Value
+							};
+
+				if (PinYins == null)
+				{
+					throw new NullReferenceException("创建 pinyin 实例 失败！");
+				}
+				//PinYins.ToDictionary(a => a.HexStr,a=>a.PinYinStr);
+				//http://referencesource.microsoft.com/#System.Core/System/Linq/Enumerable.cs,a6091311eadfdd8a
+				var dictLength = PinYins.Count();
+				Dictionary<string, string> dict = new Dictionary<string, string>(dictLength);
+				foreach (var item in PinYins)
+				{
+					dict.Add(item.hexStr, item.firstPinYin);
+				}
+				CacheDependency dep = new CacheDependency(PinYinUtils.FilePath, DateTime.Now);
+				HttpContext.Current.Cache.Insert(CacheKey, dict, dep, DateTime.MaxValue, Cache.NoSlidingExpiration, CacheItemPriority.High, null);
+				return dict;
+			}
+		}
+
+	}
+}
